e660b01741b6a0099f97577a49b8b41a3f2d05e1
Alexander Shiyan <shc_work@mail.ru>
2016-06-10 12:13:07 +0300

    10.06.2016 Init rework

--- arch/arm/boards/ccxmx51/ccxmx51.c
+++ arch/arm/boards/ccxmx51/ccxmx51.c
@@ -19,11 +19,9 @@
 #include <init.h>
 #include <net.h>
 #include <notifier.h>
-#include <platform_data/eth-fec.h>
 #include <asm/armlinux.h>
 #include <linux/sizes.h>
 #include <mach/esdctl.h>
-#include <mach/generic.h>
 #include <mach/iim.h>
 #include <mach/imx5.h>
 #include <mach/imx51-regs.h>
@@ -60,16 +58,13 @@ static const struct ccxmx51_ident {
 /* 0x13 */	{ "i.MX515@800MHz, PHY, Accel",				SZ_512M, 0, 1, 0, 0 },
 };
 
-static int ccxmx51_power_init(void)
+static u32 boardserial;
+
+static void ccxmx51_power_init(struct mc13xxx *mc13xxx)
 {
-	struct mc13xxx *mc13xxx_dev;
 	u32 val;
 
-	mc13xxx_dev = mc13xxx_get();
-	if (!mc13xxx_dev)
-		return -ENODEV;
-
-	mc13xxx_reg_read(mc13xxx_dev, MC13892_REG_POWER_MISC, &val);
+	mc13xxx_reg_read(mc13xxx, MC13892_REG_POWER_MISC, &val);
 	/* Reset devices by clearing GP01-GPO4 */
 	val &= ~((1 << 21) | (3 << 12) | (3 << 10) | (3 << 8) | (3 << 6));
 	/* Switching off the PWGT1SPIEN */
@@ -78,129 +73,128 @@ static int ccxmx51_power_init(void)
 	val &= ~(1 << 16);
 	/* Enable short circuit protection */
 	val |= (1 << 0);
-	mc13xxx_reg_write(mc13xxx_dev, MC13892_REG_POWER_MISC, val);
+	mc13xxx_reg_write(mc13xxx, MC13892_REG_POWER_MISC, val);
 
 	/* Allow charger to charge (4.2V and 560mA) */
 	val = 0x238033;
-	mc13xxx_reg_write(mc13xxx_dev, MC13892_REG_CHARGE, val);
+	mc13xxx_reg_write(mc13xxx, MC13892_REG_CHARGE, val);
 
 	if (imx_silicon_revision() < IMX_CHIP_REV_3_0) {
 		/* Set core voltage (SW1) to 1.1V */
-		mc13xxx_reg_read(mc13xxx_dev, MC13892_REG_SW_0, &val);
+		mc13xxx_reg_read(mc13xxx, MC13892_REG_SW_0, &val);
 		val &= ~0x00001f;
 		val |=  0x000014;
-		mc13xxx_reg_write(mc13xxx_dev, MC13892_REG_SW_0, val);
+		mc13xxx_reg_write(mc13xxx, MC13892_REG_SW_0, val);
 
 		/* Setup VCC (SW2) to 1.25 */
-		mc13xxx_reg_read(mc13xxx_dev, MC13892_REG_SW_1, &val);
+		mc13xxx_reg_read(mc13xxx, MC13892_REG_SW_1, &val);
 		val &= ~0x00001f;
 		val |=  0x00001a;
-		mc13xxx_reg_write(mc13xxx_dev, MC13892_REG_SW_1, val);
+		mc13xxx_reg_write(mc13xxx, MC13892_REG_SW_1, val);
 
 		/* Setup 1V2_DIG1 (SW3) to 1.25 */
-		mc13xxx_reg_read(mc13xxx_dev, MC13892_REG_SW_2, &val);
+		mc13xxx_reg_read(mc13xxx, MC13892_REG_SW_2, &val);
 		val &= ~0x00001f;
 		val |=  0x00001a;
-		mc13xxx_reg_write(mc13xxx_dev, MC13892_REG_SW_2, val);
+		mc13xxx_reg_write(mc13xxx, MC13892_REG_SW_2, val);
 	} else {
 		/* Setup VCC (SW2) to 1.225 */
-		mc13xxx_reg_read(mc13xxx_dev, MC13892_REG_SW_1, &val);
+		mc13xxx_reg_read(mc13xxx, MC13892_REG_SW_1, &val);
 		val &= ~0x00001f;
 		val |=  0x000019;
-		mc13xxx_reg_write(mc13xxx_dev, MC13892_REG_SW_1, val);
+		mc13xxx_reg_write(mc13xxx, MC13892_REG_SW_1, val);
 
 		/* Setup 1V2_DIG1 (SW3) to 1.2 */
-		mc13xxx_reg_read(mc13xxx_dev, MC13892_REG_SW_2, &val);
+		mc13xxx_reg_read(mc13xxx, MC13892_REG_SW_2, &val);
 		val &= ~0x00001f;
 		val |=  0x000018;
-		mc13xxx_reg_write(mc13xxx_dev, MC13892_REG_SW_2, val);
+		mc13xxx_reg_write(mc13xxx, MC13892_REG_SW_2, val);
 	}
 
-	if (mc13xxx_revision(mc13xxx_dev) <= MC13892_REVISION_2_0) {
+	if (mc13xxx_revision(mc13xxx) <= MC13892_REVISION_2_0) {
 		/* Set switchers in PWM mode for Atlas 2.0 and lower */
 		/* Setup the switcher mode for SW1 & SW2*/
-		mc13xxx_reg_read(mc13xxx_dev, MC13892_REG_SW_4, &val);
+		mc13xxx_reg_read(mc13xxx, MC13892_REG_SW_4, &val);
 		val &= ~0x003c0f;
 		val |=  0x001405;
-		mc13xxx_reg_write(mc13xxx_dev, MC13892_REG_SW_4, val);
+		mc13xxx_reg_write(mc13xxx, MC13892_REG_SW_4, val);
 
 		/* Setup the switcher mode for SW3 & SW4 */
-		mc13xxx_reg_read(mc13xxx_dev, MC13892_REG_SW_5, &val);
+		mc13xxx_reg_read(mc13xxx, MC13892_REG_SW_5, &val);
 		val &= ~0x000f0f;
 		val |=  0x000505;
-		mc13xxx_reg_write(mc13xxx_dev, MC13892_REG_SW_5, val);
+		mc13xxx_reg_write(mc13xxx, MC13892_REG_SW_5, val);
 	} else {
 		/* Set switchers in Auto in NORMAL mode & STANDBY mode for Atlas 2.0a */
 		/* Setup the switcher mode for SW1 & SW2*/
-		mc13xxx_reg_read(mc13xxx_dev, MC13892_REG_SW_4, &val);
+		mc13xxx_reg_read(mc13xxx, MC13892_REG_SW_4, &val);
 		val &= ~0x003c0f;
 		val |=  0x002008;
-		mc13xxx_reg_write(mc13xxx_dev, MC13892_REG_SW_4, val);
+		mc13xxx_reg_write(mc13xxx, MC13892_REG_SW_4, val);
 
 		/* Setup the switcher mode for SW3 & SW4 */
-		mc13xxx_reg_read(mc13xxx_dev, MC13892_REG_SW_5, &val);
+		mc13xxx_reg_read(mc13xxx, MC13892_REG_SW_5, &val);
 		val &= ~0x000f0f;
 		val |=  0x000808;
-		mc13xxx_reg_write(mc13xxx_dev, MC13892_REG_SW_5, val);
+		mc13xxx_reg_write(mc13xxx, MC13892_REG_SW_5, val);
 	}
 
 	/* Set VVIDEO to 2.775V, VAUDIO to 3V, VSD to 3.15V */
-	mc13xxx_reg_read(mc13xxx_dev, MC13892_REG_SETTING_1, &val);
+	mc13xxx_reg_read(mc13xxx, MC13892_REG_SETTING_1, &val);
 	val &= ~0x0001fc;
 	val |=  0x0001f4;
-	mc13xxx_reg_write(mc13xxx_dev, MC13892_REG_SETTING_1, val);
+	mc13xxx_reg_write(mc13xxx, MC13892_REG_SETTING_1, val);
 
 	/* Configure VGEN3 and VCAM regulators to use external PNP */
 	val = 0x000208;
-	mc13xxx_reg_write(mc13xxx_dev, MC13892_REG_MODE_1, val);
+	mc13xxx_reg_write(mc13xxx, MC13892_REG_MODE_1, val);
 	udelay(200);
 
 	/* Set VGEN3 to 1.8V */
-	mc13xxx_reg_read(mc13xxx_dev, MC13892_REG_SETTING_0, &val);
+	mc13xxx_reg_read(mc13xxx, MC13892_REG_SETTING_0, &val);
 	val &= ~(1 << 14);
-	mc13xxx_reg_write(mc13xxx_dev, MC13892_REG_SETTING_0, val);
+	mc13xxx_reg_write(mc13xxx, MC13892_REG_SETTING_0, val);
 
 	/* Enable VGEN3, VCAM, VAUDIO, VVIDEO, VSD regulators */
 	val = 0x049249;
-	mc13xxx_reg_write(mc13xxx_dev, MC13892_REG_MODE_1, val);
+	mc13xxx_reg_write(mc13xxx, MC13892_REG_MODE_1, val);
 
 	/* Enable USB1 charger */
 	val = 0x000409;
-	mc13xxx_reg_write(mc13xxx_dev, MC13892_REG_USB1, val);
+	mc13xxx_reg_write(mc13xxx, MC13892_REG_USB1, val);
 
 	/* Set VCOIN to 3.0V and Enable It */
-	mc13xxx_reg_read(mc13xxx_dev, MC13892_REG_POWER_CTL0, &val);
+	mc13xxx_reg_read(mc13xxx, MC13892_REG_POWER_CTL0, &val);
 	val &= ~(7 << 20);
 	val |= (4 << 20) | (1 << 23);
-	mc13xxx_reg_write(mc13xxx_dev, MC13892_REG_POWER_CTL0, val);
+	mc13xxx_reg_write(mc13xxx, MC13892_REG_POWER_CTL0, val);
 	/* Keeps VSRTC and CLK32KMCU */
 	val |= (1 << 4);
-	mc13xxx_reg_write(mc13xxx_dev, MC13892_REG_POWER_CTL0, val);
+	mc13xxx_reg_write(mc13xxx, MC13892_REG_POWER_CTL0, val);
 
 	/* De-assert reset of external devices on GP01, GPO2, GPO3 and GPO4 */
-	mc13xxx_reg_read(mc13xxx_dev, MC13892_REG_POWER_MISC, &val);
+	mc13xxx_reg_read(mc13xxx, MC13892_REG_POWER_MISC, &val);
 	/* GPO1 - External */
 	/* GP02 - LAN9221 Power */
 	/* GP03 - FEC Reset */
 	/* GP04 - Wireless Power */
-	if (IS_ENABLED(CONFIG_DRIVER_NET_SMC911X) && ccxmx51_id->eth1) {
-		struct device_node *np;
-
+	if (ccxmx51_id->eth1) {
 		val |= (1 << 8);
 		mdelay(100);
-		np = of_find_node_by_name(of_get_root_node(), "lan9221");
-		if (np)
-			of_device_enable(np);
 	}
-	if (IS_ENABLED(CONFIG_DRIVER_NET_FEC_IMX) && ccxmx51_id->eth0)
+	if (ccxmx51_id->eth0)
 		val |= (1 << 10);
 	if (ccxmx51_id->wless)
 		val |= (1 << 12);
-	mc13xxx_reg_write(mc13xxx_dev, MC13892_REG_POWER_MISC, val);
+	mc13xxx_reg_write(mc13xxx, MC13892_REG_POWER_MISC, val);
 
 	udelay(100);
 
-	return 0;
+	printf("PMIC initialized.\n");
+
+	console_flush();
+	imx51_init_lowlevel(ccxmx51_id->industrial ? 600 : 800);
+	clock_notifier_call_chain();
 }
 
 static int ccxmx51_is_compatible(void)
@@ -208,26 +202,47 @@ static int ccxmx51_is_compatible(void)
 	return of_machine_is_compatible("digi,connectcore-ccxmx51-som");
 }
 
-/*
- * On this board the SDRAM is always configured for 512Mib. The real
- * size is determined by the board id read from the IIM module.
- */
+static int ccxmx51_board_fixup(struct device_node *root, void *unused)
+{
+	char *serial;
+
+	if (!ccxmx51_id->eth1) {
+		struct device_node *np;
+		np = of_find_node_by_name(root, "lan9221@5,0");
+		if (np)
+			of_device_disable(np);
+	}
+
+	serial = basprintf("%08x%08x", 0, boardserial);
+	of_set_property(root, "serial-number", serial, strlen(serial) + 1, 1);
+
+	return 0;
+}
+
 static int ccxmx51_sdram_fixup(void)
 {
-	if (ccxmx51_is_compatible())
-		imx_esdctl_disable();
+	if (!ccxmx51_is_compatible())
+		return 0;
+
+	/*
+	 * On this board the SDRAM is always configured for 512Mib. The real
+	 * size is determined by the board id read from the IIM module.
+	 */
+	imx_esdctl_disable();
+
+	of_register_fixup(ccxmx51_board_fixup, NULL);
 
 	return 0;
 }
 postcore_initcall(ccxmx51_sdram_fixup);
 
-static int ccxmx51_devices_init(void)
+static int ccxmx51_init(void)
 {
-	u8 hwid[6];
 	char manloc;
+	u8 hwid[6];
 
 	if (!ccxmx51_is_compatible())
-		return -ENOSYS;
+		return 0;
 
 	if ((imx_iim_read(1, 9, hwid, sizeof(hwid)) != sizeof(hwid)) || (hwid[0] < 0x02) || (hwid[0] >= ARRAY_SIZE(ccxmx51_ids)))
 		memset(hwid, 0x00, sizeof(hwid));
@@ -237,6 +252,7 @@ static int ccxmx51_devices_init(void)
 
 	if (hwid[0]) {
 		printf("Module HW Rev : %02x\n", hwid[1] + 1);
+
 		switch (hwid[2] & 0xc0) {
 		case 0x00:
 			manloc = 'B';
@@ -251,23 +267,25 @@ static int ccxmx51_devices_init(void)
 			manloc = 'N';
 			break;
 		}
-		printf("Module Serial : %c%d\n", manloc, ((hwid[2] & 0x3f) << 24) | (hwid[3] << 16) | (hwid[4] << 8) | hwid[5]);
-		if ((ccxmx51_id->mem_sz - SZ_128M) > 0)
-			arm_add_mem_device("ram1", MX51_CSD0_BASE_ADDR + SZ_128M, ccxmx51_id->mem_sz - SZ_128M);
-	} else
-		return -ENOSYS;
 
-	if (ccxmx51_power_init()) {
-		printf("Could not setup PMIC. System unusable.\n");
-		return -ENODEV;
+		eth_register_ethaddr(0, hwid);
+
+		boardserial = ((hwid[2] & 0x3f) << 24) | (hwid[3] << 16) | (hwid[4] << 8) | hwid[5];
+		printf("Module Serial : %c%d\n", manloc, boardserial);
+
+		if ((ccxmx51_id->mem_sz - SZ_128M) > 0)
+			arm_add_mem_device("ram1", MX51_CSD0_BASE_ADDR + SZ_128M,
+					   ccxmx51_id->mem_sz - SZ_128M);
+
+		mc13xxx_register_init_callback(ccxmx51_power_init);
+	} else {
+		printf("Machine is unsupported. System halted.\n");
+		hang();
 	}
 
-	console_flush();
-	imx51_init_lowlevel(ccxmx51_id->industrial ? 600 : 800);
-	clock_notifier_call_chain();
-
-	eth_register_ethaddr(0, hwid);
+	barebox_set_model("Digi ConnectCore i.MX51");
+	barebox_set_hostname("ccmx51");
 
 	return 0;
 }
-device_initcall(ccxmx51_devices_init);
+device_initcall(ccxmx51_init);
