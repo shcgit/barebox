6fbc6ea34688f83975f41f14ea016b0354061caf
Alexander Shiyan <shc_work@mail.ru>
2016-07-02 07:48:48 +0300

    Revert "net: smc911x: Add parsing devicetree options"
    This reverts commit 57dcaaba2aa1ef493b810d3fb746c434fe55d96f.

--- drivers/net/smc911x.c
+++ drivers/net/smc911x.c
@@ -34,15 +34,16 @@
 #include "smc911x.h"
 
 struct smc911x_priv {
-	struct eth_device	edev;
-	struct mii_bus		miibus;
-	void __iomem		*base;
+	struct eth_device edev;
+	struct mii_bus miibus;
+	void __iomem *base;
 
-	u32			shift;
-	u32			flags;
-	u32			phy_mask;
-	u32			idrev;
-	unsigned int		using_extphy;
+	int shift;
+	int generation;
+	unsigned int flags;
+	unsigned int idrev;
+	unsigned int using_extphy;
+	unsigned int phy_mask;
 
 	u32 (*reg_read)(struct smc911x_priv *priv, u32 reg);
 	void (*reg_write)(struct smc911x_priv *priv, u32 reg, u32 val);
@@ -496,12 +497,16 @@ static int smc911x_probe(struct device_d *dev)
 	struct resource *iores;
 	struct eth_device *edev;
 	struct smc911x_priv *priv;
-	u32 val, generation;
-	int ret;
+	uint32_t val;
+	int is_32bit, ret;
 	struct smc911x_plat *pdata = dev->platform_data;
 
 	priv = xzalloc(sizeof(*priv));
-
+	is_32bit = dev->resource[0].flags & IORESOURCE_MEM_TYPE_MASK;
+	if (!is_32bit)
+		is_32bit = 1;
+	else
+		is_32bit = is_32bit == IORESOURCE_MEM_32BIT;
 	iores = dev_request_mem_resource(dev, 0);
 	if (IS_ERR(iores))
 		return PTR_ERR(iores);
@@ -511,23 +516,9 @@ static int smc911x_probe(struct device_d *dev)
 		priv->shift = pdata->shift;
 		priv->flags = pdata->flags;
 		priv->phy_mask = pdata->phy_mask;
-	} else if (IS_ENABLED(CONFIG_OFDEVICE) && dev->device_node) {
-		ret = of_property_read_u32(dev->device_node, "reg-io-width", &val);
-		if (ret)
-			return ret;
-		if (val == 4)
-			priv->flags |= SMC911X_USE_32BIT;
-
-		of_property_read_u32(dev->device_node, "reg-shift", &priv->shift);
-
-		if (of_property_read_bool(dev->device_node, "smsc,force-internal-phy"))
-			priv->flags |= SMC911X_FORCE_INTERNAL_PHY;
-
-		if (of_property_read_bool(dev->device_node, "smsc,force-external-phy"))
-			priv->flags |= SMC911X_FORCE_EXTERNAL_PHY;
 	}
 
-	if (priv->flags & SMC911X_USE_32BIT) {
+	if (is_32bit) {
 		if (priv->shift) {
 			priv->reg_read = __smc911x_reg_readl_shift;
 			priv->reg_write = __smc911x_reg_writel_shift;
@@ -571,8 +562,7 @@ static int smc911x_probe(struct device_d *dev)
 	if (val != 0x87654321) {
 		dev_err(dev, "no smc911x found on 0x%p (byte_test=0x%08x)\n",
 			priv->base, val);
-		if ((((val >> 16) & 0xFFFF) == (val & 0xFFFF)) &&
-		    (priv->flags & SMC911X_USE_32BIT)) {
+		if (((val >> 16) & 0xFFFF) == (val & 0xFFFF)) {
 			/*
 			 * This may mean the chip is set
 			 * for 32 bit while the bus is reading 16 bit
@@ -590,7 +580,7 @@ static int smc911x_probe(struct device_d *dev)
 	case 0x01150000:
 	case 0x218A0000:
 		/* LAN911[5678] family */
-		generation = val & 0x0000FFFF;
+		priv->generation = val & 0x0000FFFF;
 		break;
 
 	case 0x118A0000:
@@ -598,7 +588,7 @@ static int smc911x_probe(struct device_d *dev)
 	case 0x116A0000:
 	case 0x115A0000:
 		/* LAN921[5678] family */
-		generation = 3;
+		priv->generation = 3;
 		break;
 
 	case 0x92100000:
@@ -606,7 +596,7 @@ static int smc911x_probe(struct device_d *dev)
 	case 0x92200000:
 	case 0x92210000:
 		/* LAN9210/LAN9211/LAN9220/LAN9221 */
-		generation = 4;
+		priv->generation = 4;
 		break;
 
 	default:
@@ -616,7 +606,7 @@ static int smc911x_probe(struct device_d *dev)
 	}
 
 	dev_info(dev, "LAN911x identified, idrev: 0x%08X, generation: %d\n",
-		   val, generation);
+		   val, priv->generation);
 
 	edev = &priv->edev;
 	edev->priv = priv;
