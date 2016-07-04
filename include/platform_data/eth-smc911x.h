3a98133007043700ce8b814b825d97ad9c4aa854
Alexander Shiyan <shc_work@mail.ru>
2016-07-04 11:41:58 +0300

    Eth+, Coincell-

--- include/platform_data/eth-smc911x.h
+++ include/platform_data/eth-smc911x.h
@@ -12,14 +12,13 @@
  * Pass pointer to this structure as part of device_d -> platform_data
  */
 struct smc911x_plat {
-	u32	shift;
-	u32	flags;
-	u32	phy_mask;	/* external PHY only: mask out PHYs,
+	u32 shift;
+	unsigned int flags;
+	unsigned int phy_mask;	/* external PHY only: mask out PHYs,
 				   e.g. ~(1 << 5) to use PHY addr 5 */
 };
 
 #define SMC911X_FORCE_INTERNAL_PHY	0x01
 #define SMC911X_FORCE_EXTERNAL_PHY	0x02
-#define SMC911X_USE_32BIT		0x04
 
 #endif /* __SMC911X_PLATFORM_H_ */
