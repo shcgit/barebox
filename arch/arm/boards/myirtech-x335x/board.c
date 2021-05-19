cfa97084c82e3104f3c5196dbedd529ba103971a
Alexander Shiyan <shc_work@mail.ru>
2021-05-19 10:40:48 +0300

    Fix two codec for AM335x. Introduce runtime patch for this.

--- arch/arm/boards/myirtech-x335x/board.c
+++ arch/arm/boards/myirtech-x335x/board.c
@@ -24,13 +24,63 @@
 #include <mach/gpmc.h>
 #include <linux/err.h>
 #include <mach/am33xx-generic.h>
+#include <i2c/i2c.h>
 
 static struct omap_barebox_part myir_barebox_part = {
 	.nand_offset = SZ_128K,
 	.nand_size = SZ_512K,
 };
 
-static __init int beaglebone_devices_init(void)
+static void myir_disable_device(struct device_node *root, const char *label)
+{
+	struct device_node *np = of_find_node_by_name(root, label);
+	if (np)
+		of_device_disable(np);
+}
+
+static int myir_probe_i2c(struct i2c_adapter *adapter, int addr)
+{
+	u8 buf[1];
+	struct i2c_msg msg = {
+		.addr = addr,
+		.buf = buf,
+		.len = sizeof(buf),
+		.flags = I2C_M_RD,
+	};
+
+	return (i2c_transfer(adapter, &msg, 1) == 1) ? 0 : -ENODEV;
+}
+
+#define SGTL5000_ADDR	0x0a
+#define AIC3100_ADDR	0x18
+
+static int myir_board_fixup(struct device_node *root, void *unused)
+{
+	struct i2c_adapter *adapter = i2c_get_adapter(0);
+	if (!adapter)
+		return -ENODEV;
+
+	if (myir_probe_i2c(adapter, SGTL5000_ADDR))
+		myir_disable_device(root, "sound");
+	else if (myir_probe_i2c(adapter, AIC3100_ADDR))
+		myir_disable_device(root, "sound1");
+
+	return 0;
+}
+
+static __init int myir_i2c_fixup(void)
+{
+	if (!of_machine_is_compatible("milas,informer-am335x") &&
+	    !of_machine_is_compatible("milas,spider-am335x"))
+		return 0;
+
+	of_register_fixup(myir_board_fixup, NULL);
+
+	return 0;
+}
+postcore_initcall(myir_i2c_fixup);
+
+static __init int myir_devices_init(void)
 {
 	if (!of_machine_is_compatible("myir,myc-am335x"))
 		return 0;
@@ -56,4 +106,4 @@ static __init int beaglebone_devices_init(void)
 
 	return 0;
 }
-coredevice_initcall(beaglebone_devices_init);
+coredevice_initcall(myir_devices_init);
