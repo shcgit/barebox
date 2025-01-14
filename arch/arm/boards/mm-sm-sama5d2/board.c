3fdf9486029aead5c38e649d696d667174646059
Alexander Shiyan <eagle.alexander923@gmail.com>
2025-01-14 13:08:46 +0300

    sama5d2: Support boot variants

--- arch/arm/boards/mm-sm-sama5d2/board.c
+++ arch/arm/boards/mm-sm-sama5d2/board.c
@@ -1,18 +1,35 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
-#include <init.h>
+#include <bbu.h>
+#include <bootsource.h>
+#include <common.h>
 #include <envfs.h>
-//#include <bbu.h>
+#include <init.h>
 #include <of.h>
 
-static int mm_sm_sama5d2_init(void)
+static int __init mm_sm_sama5d2_init(void)
 {
+	enum bootsource bootsource;
+	int instance;
+
 	if (!of_machine_is_compatible("milas,mm-sm-sama5d2"))
 		return 0;
 
-//	bbu_register_std_file_update("microSD", BBU_HANDLER_FLAG_DEFAULT,
-//				     "/mnt/mmc1.0/barebox.bin",
-//				     filetype_arm_barebox);
+	bootsource = bootsource_get();
+	instance = bootsource_get_instance();
+
+	if (bootsource != BOOTSOURCE_MMC || !instance) {
+		if (bootsource != BOOTSOURCE_SPI) {
+			pr_warn("Boot source: %s, instance %i\n",
+				bootsource_to_string(bootsource), instance);
+		} else
+			of_device_enable_path("/chosen/environment-qspi");
+	} else
+		of_device_enable_path("/chosen/environment-sd");
+
+	bbu_register_std_file_update("sd", BBU_HANDLER_FLAG_DEFAULT,
+				     "/mnt/mmc1.0/barebox.bin",
+				     filetype_arm_barebox);
 
 	defaultenv_append_directory(defaultenv_mm_sm_sama5d2);
 
